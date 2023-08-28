599bc76740bb467d0960a66b57bf2b3108d68cff
Stjepan Obrankovic <stjepano@gmail.com>
2023-08-28 21:27:17 +0200

    Change highlighting for selected buffer.

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,1 +1,14 @@
-require("bufferline").setup()
+local bufferline = require('bufferline')
+bufferline.setup {
+  options = {
+    style_preset = bufferline.style_preset.default,
+    numbers = "none",
+    separator_style = "thin",
+  },
+  highlights = {
+    buffer_selected = {
+      fg = "#ffa500",
+      bold = true,
+    },
+  },
+}
