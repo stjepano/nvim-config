1a6dce4ccb27d45bbcbc71b71c5e508fba9f212a
Stjepan Obrankovic <stjepano@gmail.com>
2023-12-26 10:05:19 +0100

    Reconfigure dapui layout

--- after/plugin/nvim-dap-ui.lua
+++ after/plugin/nvim-dap-ui.lua
@@ -1,4 +1,25 @@
-require("dapui").setup()
+require("dapui").setup({
+  layouts = { {
+      elements = {
+        {
+          id = "scopes",
+          size = 1.0
+        },
+      },
+      position = "right",
+      size = 60
+    }, {
+      elements = { {
+          id = "repl",
+          size = 0.5
+        }, {
+          id = "console",
+          size = 0.5
+        } },
+      position = "bottom",
+      size = 10
+    } },
+})
 
 local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
